5ff99947bfe714155de9c9047679f21f04db97f7
Minyong Oh <dhalsdyd@gmail.com>
2022-03-27 13:56:51 +0900

    Add : multiparty packakge

--- routes/upload_image.js
+++ routes/upload_image.js
@@ -1,19 +1,24 @@
 var express = require('express');
+var multiparty = require("multiparty")
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.render('upload/upload-image');
-  });
+});
   
 router.post('/image', function(req, res, next) {
-res.redirect(303,'/upload/success')
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.vacationPhotoContestProcessError(req, res, err.message)
+    res.redirect(303,'/upload/success')
+  })
 });
 
 router.get('/success', function(req, res, next) {
-res.render('upload/upload-success');
+  res.render('upload/upload-success');
 });
 router.get('/error', function(req, res, next) {
-res.render('upload/upload-error');
+  res.render('upload/upload-error');
 });
 
 module.exports = router;
